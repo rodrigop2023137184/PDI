98055e28ea449818df792154d2956604a2ce6a74
rodrigop2023137184 <a2023137184@alumni.iscac.pt>
2026-03-19 22:15:51 +0000

    Delete App.tsx

--- App.tsx
+++
@@ -1,7 +0,0 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import NevegadorApp from './src/navegacao/NavegadorApp';
-
-export default function App() {
-  return <NevegadorApp />;
-}
